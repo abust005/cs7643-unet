b068c60048b75e800b5f0a2db5fd1df207185447
Jon Pang <jonpang13@gmail.com>
2025-06-28 13:15:09 -0400

    Create conda environment file and update file formatting with black

--- model/unet_blocks.py
+++ model/unet_blocks.py
@@ -3,70 +3,93 @@ from torchvision.transforms.functional import center_crop
 import torch.nn as nn
 import torch.nn.init as init
 
-'''
+"""
   U-Net modules as described in https://arxiv.org/abs/1505.04597
   See Section 2: Network Architecture
-'''
+"""
+
 
 class ConvBlock(nn.Module):
-  def __init__(self, in_channels, out_channels, padding='same', padding_mode='reflect'):
-    super().__init__()
+    def __init__(
+        self, in_channels, out_channels, padding="same", padding_mode="reflect"
+    ):
+        super().__init__()
 
-    self.module_list = nn.Sequential(
-      nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, 
-                padding=padding, padding_mode=padding_mode, stride=1),
-      nn.ReLU(),
-      nn.Conv2d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, padding=padding, padding_mode=padding_mode, stride=1),
-      nn.ReLU(),
-    )
-    
-    for layer in self.module_list:
-        if isinstance(layer, nn.Conv2d):
-            init.kaiming_normal_(layer.weight, mode='fan_out', nonlinearity='relu')
-            if layer.bias is not None:
-                init.zeros_(layer.bias)
+        self.module_list = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                padding=padding,
+                padding_mode=padding_mode,
+                stride=1,
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                kernel_size=3,
+                padding=padding,
+                padding_mode=padding_mode,
+                stride=1,
+            ),
+            nn.ReLU(),
+        )
 
-  def forward(self, x):
-    return self.module_list.forward(x)
+        for layer in self.module_list:
+            if isinstance(layer, nn.Conv2d):
+                init.kaiming_normal_(layer.weight, mode="fan_out", nonlinearity="relu")
+                if layer.bias is not None:
+                    init.zeros_(layer.bias)
+
+    def forward(self, x):
+        return self.module_list.forward(x)
+
 
 class DownConv(nn.Module):
-  def __init__(self, in_channels, out_channels, padding, padding_mode):
-    super().__init__()
+    def __init__(self, in_channels, out_channels, padding, padding_mode):
+        super().__init__()
 
-    self.conv = ConvBlock(in_channels, out_channels, padding, padding_mode)
-    self.pool = nn.MaxPool2d(2, stride=2)
-    self.dropout = nn.Dropout2d(0.3)
+        self.conv = ConvBlock(in_channels, out_channels, padding, padding_mode)
+        self.pool = nn.MaxPool2d(2, stride=2)
+        self.dropout = nn.Dropout2d(0.3)
 
-  def forward(self, x):
+    def forward(self, x):
 
-    x_crop = self.conv(x)
-    x_out = self.pool(x_crop)
-    x_out = self.dropout(x_out)
+        x_crop = self.conv(x)
+        x_out = self.pool(x_crop)
+        x_out = self.dropout(x_out)
 
-    return x_crop, x_out
+        return x_crop, x_out
+
 
 class UpConv(nn.Module):
-  def __init__(self, in_channels):
-    super().__init__()
+    def __init__(self, in_channels):
+        super().__init__()
 
-    self.module_list = nn.ModuleDict(
-      {
-        'up_conv' : nn.ConvTranspose2d(in_channels=in_channels, out_channels=in_channels//2, kernel_size=2, stride=2),
-        'conv_block' : ConvBlock(in_channels, out_channels= in_channels//2),
-        'dropout' : nn.Dropout2d(0.3)
-      }
-    )
+        self.module_list = nn.ModuleDict(
+            {
+                "up_conv": nn.ConvTranspose2d(
+                    in_channels=in_channels,
+                    out_channels=in_channels // 2,
+                    kernel_size=2,
+                    stride=2,
+                ),
+                "conv_block": ConvBlock(in_channels, out_channels=in_channels // 2),
+                "dropout": nn.Dropout2d(0.3),
+            }
+        )
 
-  def forward(self, x1, x2):
-    '''
-      x1 : output of previous layer
-      x2 : output from corresponding down-conv 
-           to be cropped and concatenated
-    '''
+    def forward(self, x1, x2):
+        """
+        x1 : output of previous layer
+        x2 : output from corresponding down-conv
+             to be cropped and concatenated
+        """
 
-    x1 = self.module_list['up_conv'](x1)
-    x1_img_shape = [x1.shape[-2], x1.shape[-1]]
+        x1 = self.module_list["up_conv"](x1)
+        x1_img_shape = [x1.shape[-2], x1.shape[-1]]
 
-    x3 = torch.cat((x1, center_crop(x2, x1_img_shape)), dim=1)
+        x3 = torch.cat((x1, center_crop(x2, x1_img_shape)), dim=1)
 
-    return self.module_list['dropout'](self.module_list['conv_block'](x3))
+        return self.module_list["dropout"](self.module_list["conv_block"](x3))
--- viz_data.py
+++ viz_data.py
@@ -4,49 +4,53 @@ from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from torchvision.transforms.functional import center_crop
 
-# set SHOW_EMPTY_MASK=True when you want to see images even when there is no 
+# set SHOW_EMPTY_MASK=True when you want to see images even when there is no
 # applicable segmentation mask (i.e., a clean image)
 SHOW_EMPTY_MASK = True
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-  train_files = None
-  val_files = None
-  data_dir = None
-  batch_size = 8
-  image_dim = (240, 240)
+    train_files = None
+    val_files = None
+    data_dir = None
+    batch_size = 8
+    image_dim = (240, 240)
 
-  training_dataloader = DataLoader(BraTS2020Dataset(), batch_size=batch_size, shuffle=False)
-  # training_dataloader = DataLoader(BraTS2020Dataset(transform=PyTMinMaxScalerVectorized), batch_size=batch_size, shuffle=False)
+    training_dataloader = DataLoader(
+        BraTS2020Dataset(), batch_size=batch_size, shuffle=False
+    )
+    # training_dataloader = DataLoader(BraTS2020Dataset(transform=PyTMinMaxScalerVectorized), batch_size=batch_size, shuffle=False)
 
-  for b in range(0, 10):
-      trainX, trainY = next(iter(training_dataloader))
-      for i in range(0, batch_size):
-        if (torch.max(trainY[i]) > 0) or (SHOW_EMPTY_MASK):
-          
-          for c in range(trainY[i].shape[0]):
-            trainY[i, c, :, :] *= c
+    for b in range(0, 10):
+        trainX, trainY = next(iter(training_dataloader))
+        for i in range(0, batch_size):
+            if (torch.max(trainY[i]) > 0) or (SHOW_EMPTY_MASK):
 
-          mask = torch.sum(trainY[i], dim=0, keepdim=False)  # Example: Convert RGB to grayscale
+                for c in range(trainY[i].shape[0]):
+                    trainY[i, c, :, :] *= c
 
-          fig, axs = plt.subplots(3, 2, figsize=(10, 10))
-        #   fig.delaxes(axs[2,1])
-          axs[0, 0].imshow(torch.moveaxis(mask, 0, -1))
-          axs[0, 1].imshow(torch.moveaxis(center_crop(mask, [52, 52]), 0, -1))
-          axs[1, 0].imshow(trainX[i, 0, :, :])
-          axs[1, 1].imshow(trainX[i, 1, :, :])
-          axs[2, 0].imshow(trainX[i, 2, :, :])
-          axs[2, 1].imshow(trainX[i, 3, :, :])
-          
-          axs[0, 0].set_title('Mask') 
-          axs[0, 1].set_title('Cropped Mask') 
-          axs[1, 0].set_title('T1') 
-          axs[1, 1].set_title('T1Gd') 
-          axs[2, 0].set_title('T2') 
-          axs[2, 1].set_title('T2-FLAIR') 
+                mask = torch.sum(
+                    trainY[i], dim=0, keepdim=False
+                )  # Example: Convert RGB to grayscale
 
-          plt.tight_layout()
+                fig, axs = plt.subplots(3, 2, figsize=(10, 10))
+                #   fig.delaxes(axs[2,1])
+                axs[0, 0].imshow(torch.moveaxis(mask, 0, -1))
+                axs[0, 1].imshow(torch.moveaxis(center_crop(mask, [52, 52]), 0, -1))
+                axs[1, 0].imshow(trainX[i, 0, :, :])
+                axs[1, 1].imshow(trainX[i, 1, :, :])
+                axs[2, 0].imshow(trainX[i, 2, :, :])
+                axs[2, 1].imshow(trainX[i, 3, :, :])
 
-          fig.show()
-          plt.waitforbuttonpress()
-          plt.close(fig)
+                axs[0, 0].set_title("Mask")
+                axs[0, 1].set_title("Cropped Mask")
+                axs[1, 0].set_title("T1")
+                axs[1, 1].set_title("T1Gd")
+                axs[2, 0].set_title("T2")
+                axs[2, 1].set_title("T2-FLAIR")
+
+                plt.tight_layout()
+
+                fig.show()
+                plt.waitforbuttonpress()
+                plt.close(fig)
