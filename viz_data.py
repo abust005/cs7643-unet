256f6958728def85101766e4f7831d2242cadef1
Adriel Bustamante <abustamante31@gatech.edu>
2025-07-05 00:07:10 -0700

    Update viz_data to use data transforms, omit cropped mask

--- viz_data.py
+++ viz_data.py
@@ -3,6 +3,7 @@ from data.dataset import BraTS2020Dataset, PyTMinMaxScalerVectorized
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from torchvision.transforms.functional import center_crop
+from torchvision.transforms import Compose, v2
 from matplotlib.colors import BoundaryNorm
 
 # set SHOW_EMPTY_MASK=True when you want to see images even when there is no
@@ -16,13 +17,18 @@ if __name__ == "__main__":
     data_dir = None
     batch_size = 8
     image_dim = (240, 240)
-
+    transform = Compose([v2.RandomRotation(45),
+                         v2.RandomHorizontalFlip(0.15),
+                         v2.RandomVerticalFlip(0.15),
+                         v2.ElasticTransform()])
+    
     training_dataloader = DataLoader(
-        BraTS2020Dataset(), batch_size=batch_size, shuffle=False
+        BraTS2020Dataset(transform=transform), batch_size=batch_size, shuffle=False
     )
     # training_dataloader = DataLoader(BraTS2020Dataset(transform=PyTMinMaxScalerVectorized), batch_size=batch_size, shuffle=False)
 
     b_norm = BoundaryNorm(torch.arange(0, 5), ncolors=4)
+    reflection_pad_fn = torch.nn.ReflectionPad1d(68)
 
     for b in range(0, 10):
         trainX, trainY, singleY = next(iter(training_dataloader))
@@ -37,17 +43,18 @@ if __name__ == "__main__":
                     norm=b_norm,
                 )
                 axs[0, 1].imshow(
-                    torch.moveaxis(center_crop(singleY[i], [52, 52]), 0, -1),
+                    torch.moveaxis(trainY[i,1:4], 0, -1),
                     cmap=plt.get_cmap("tab10"),
                     norm=b_norm,
                 )
+
                 axs[1, 0].imshow(trainX[i, 0, :, :])
                 axs[1, 1].imshow(trainX[i, 1, :, :])
                 axs[2, 0].imshow(trainX[i, 2, :, :])
                 axs[2, 1].imshow(trainX[i, 3, :, :])
 
                 axs[0, 0].set_title("Mask")
-                axs[0, 1].set_title("Cropped Mask")
+                axs[0, 1].set_title("Split Mask")
                 axs[1, 0].set_title("T1")
                 axs[1, 1].set_title("T1Gd")
                 axs[2, 0].set_title("T2")
